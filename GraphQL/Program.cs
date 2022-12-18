15a2d4691043d9c44d83d77d66bf17c9efa02547
Gavin <checkinnuggets@users.noreply.github.com>
2022-12-18 13:11:20 +0000

    Session 3 complete.

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,14 +1,19 @@
 using ConferencePlanner.GraphQL;
 using ConferencePlanner.GraphQL.Data;
+using ConferencePlanner.GraphQL.DataLoader;
+using ConferencePlanner.GraphQL.Types;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+builder.Services.AddPooledDbContextFactory<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>()
-    .AddMutationType<Mutation>();
+    .AddMutationType<Mutation>()
+    .AddType<SpeakerType>()
+    .AddDataLoader<SpeakerByIdDataLoader>()
+    .AddDataLoader<SessionByIdDataLoader>();
 
 var app = builder.Build();
 
--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -1,12 +1,23 @@
-using System.Linq;
-using HotChocolate;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using ConferencePlanner.GraphQL.Data;
+using ConferencePlanner.GraphQL.DataLoader;
+using HotChocolate;
+
 
 namespace ConferencePlanner.GraphQL
 {
     public class Query
     {
-        public IQueryable<Speaker> GetSpeakers([Service] ApplicationDbContext context) 
-            => context.Speakers;
+        [UseApplicationDbContext]
+        public Task<List<Speaker>> GetSpeakers([ScopedService] ApplicationDbContext context) 
+            => context.Speakers.ToListAsync();
+
+        public Task<Speaker> GetSpeakerAsync(
+            int id,
+            SpeakerByIdDataLoader dataLoader,
+            CancellationToken cancellationToken) =>
+            dataLoader.LoadAsync(id, cancellationToken);
     }
 }
