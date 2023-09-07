0358ac117028a695715c29ed1721738063a4237b
Gavin <checkinnuggets@users.noreply.github.com>
2023-09-07 11:19:37 +0100

    Reworked to use new style dataloaders.

---
+++ GraphQL/DataLoader/DataLoaders.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using ConferencePlanner.GraphQL.Data;
+
+namespace ConferencePlanner.GraphQL.DataLoader
+{
+
+    public static class DataLoaders
+    {
+        [DataLoader]
+        public static async Task<IReadOnlyDictionary<int, Attendee>> AttendeeByIdDataLoader(
+            IReadOnlyList<int> keys,
+            ApplicationDbContext context,
+            CancellationToken cancellationToken)
+        {
+            return await context.Attendees
+                            .Where(a => keys.Contains(a.Id))
+                            .ToDictionaryAsync(a => a.Id, cancellationToken);
+        }
+
+        [DataLoader]
+        public static async Task<IReadOnlyDictionary<int, Session>> SessionByIdDataLoader(
+            IReadOnlyList<int> keys,
+            ApplicationDbContext context,
+            CancellationToken cancellationToken)
+        {
+            return await context.Sessions
+                .Where(a => keys.Contains(a.Id))
+                .ToDictionaryAsync(a => a.Id, cancellationToken);
+        }
+
+        [DataLoader]
+        public static async Task<IReadOnlyDictionary<int, Speaker>> SpeakerByIdDataLoader(
+            IReadOnlyList<int> keys,
+            ApplicationDbContext context,
+            CancellationToken cancellationToken)
+        {
+            return await context.Speakers
+                .Where(a => keys.Contains(a.Id))
+                .ToDictionaryAsync(a => a.Id, cancellationToken);
+        }
+
+        [DataLoader]
+        public static async Task<IReadOnlyDictionary<int, Track>> TrackByIdDataLoader(
+            IReadOnlyList<int> keys,
+            ApplicationDbContext context,
+            CancellationToken cancellationToken)
+        {
+            return await context.Tracks
+                .Where(a => keys.Contains(a.Id))
+                .ToDictionaryAsync(a => a.Id, cancellationToken);
+        }
+    }
+}
